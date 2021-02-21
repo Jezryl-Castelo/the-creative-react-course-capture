5318ddd1d782b448b9690223effb8e2a18f91635
Jezryl <yamabags@gmail.com>
2021-02-21 10:49:25 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AboutUs from './pages/AboutUs';
+//src/pages/AboutUs.js
+
+function App() {
+  return (
+    <div className="App">
+     <AboutUs />
+    </div>
+  );
+}
+
+export default App;
